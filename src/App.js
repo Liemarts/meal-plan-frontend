0ec35978df5ea8f9690e63c4aa3dbed8a70af1f7
Liemarts <liemarts8@gmail.com>
2025-03-15 21:37:47 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import { MyMeals } from './MyMeals';
+import { getAllMeals } from './FetchMeals';
+
+function App() {
+
+  const [myMeal, setMeal] = useState ([]);
+
+  useEffect (() => {
+    getAllMeals(setMeal)
+  }, [])
+
+
+  return (
+      <div>
+
+        <h1>Meal Plan</h1>
+
+        <input 
+        type="text"
+        placeholder="Add a meal" 
+        />
+
+        <button>
+          Add
+        </button>
+
+        {/* <MyMeals text="WE GOT HERE!!!" /> */}
+
+        {myMeal.map((meal) => <MyMeals text={meal.title}/>)}
+        
+
+      </div>
+  );
+}
+
+export default App;
